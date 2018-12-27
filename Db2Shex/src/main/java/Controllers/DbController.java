e7f343d5b3e3da836b11c9ddcf2e69c71b8fbd61
jose <jose@martin>
2018-12-27 17:38:38 +0100

    hibernate

--- Db2Shex/src/main/java/Controllers/DbController.java
+++ Db2Shex/src/main/java/Controllers/DbController.java
@@ -1,6 +1,8 @@
 package Controllers;
 
 //https://github.com/spring-guides?page=2
+//https://www.callicoder.com/spring-boot-file-upload-download-jpa-hibernate-mysql-database-example/
+//https://github.com/husseinterek/spring-boot-jpa-hibernate/blob/master/src/main/resources/application.properties
 
 public class DbController {
 
