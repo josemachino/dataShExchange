c0003e928dcaca6c1d3a022393e6a3a3760d94cc
jose <jose@martin>
2019-01-10 17:09:36 +0100

    antlr working

--- Db2Shex/src/main/java/Application.java
+++ Db2Shex/src/main/java/Application.java
@@ -1,9 +1,20 @@
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import storage.StorageService;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	@Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
