82b18e0456b06f640c5b2907f6efa0d88b7cba4a
jose <jose@martin>
2019-01-21 14:47:33 +0100

    a

--- Db2Shex/src/main/java/des/models/TGDS.java
+++ Db2Shex/src/main/java/des/models/TGDS.java
@@ -3,23 +3,39 @@ package des.models;
 import java.util.Map;
 
 public class TGDS {
-	public TGDS(Map<String, String> functions, Rule[] rules) {
+	public TGDS(Map<String, String> functions, Object rules) {
 		super();
 		this.functions = functions;
 		this.rules = rules;
 	}
 	private Map<String,String> functions;
-	private Rule[] rules;
+	private Object rules;
 	public Map<String, String> getFunctions() {
 		return functions;
 	}
 	public void setFunctions(Map<String, String> functions) {
 		this.functions = functions;
 	}
-	public Rule[] getRules() {
+	public Object getRules() {
 		return rules;
 	}
 	public void setRules(Rule[] rules) {
 		this.rules = rules;
 	}
+	/*private Object functions;
+	private Object rules;
+	public Object getFunctions() {
+		return functions;
+	}
+	public void setFunctions(Object functions) {
+		this.functions = functions;
+	}
+	public Object getRules() {
+		return rules;
+	}
+	public void setRules(Object rules) {
+		this.rules = rules;
+	}*/
+
+	
 }
