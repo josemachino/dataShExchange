15e67a2ae96e4602b6947f2d766d9664e58696cf
jose <jose@martin>
2019-01-21 17:25:22 +0100

    query

--- Db2Shex/src/main/resources/static/uiloadjs/loadExchangeView.js
+++ Db2Shex/src/main/resources/static/uiloadjs/loadExchangeView.js
@@ -32,8 +32,30 @@ exchange:function(e){
 			}        	
     });
 	
+	//construct sql query
+	let tgds=editorJSON.get();
+	let TyName="TypesFact";
+	let TriName="Triples";
+	let c1="create table Triples(s varchar,p varchar, o varchar)"
+	let c2="create table TypesFact(term varchar,type varchar)"
+	tgds.yield.forEach(function(atom){
+		var q=""		
+		if (atom.args.length==1){
+			q.concat.("insert into").concat(" ").concat(TyName).concat(" ").concat("(").concat("term,type").concat(")");
+			//consider that the length of args in case of type atom will allays be one 			
+			q.concat("SELECT").concat(" ").concat("CONCAT(").concat(tgds.functions[atom.args[0].function]).concat(",").concat(atom.args[0].args[0].attr).concat(")").concat(",").concat(atom.atom).concat("FROM").concat(" ").concat(atom.args[0].args[0].rel);
+			q.concat(";")
+			
+		}else if (atom.args.length==3){//it is the triple atom
+			q.concat.("insert into").concat(" ").concat(TriName).concat(" ").concat("(").concat("term,type").concat(")");
+			atom.args.forEach(function(term){
+				q.concat("SELECT").concat(" ").concat("CONCAT(").concat(tgds.functions[atom.args[0].function]).concat(",").concat(atom.args[0].args[0].attr).concat(")")
+			})
+		}
+	})
+	[[{"function":"f1","args":[{"rel":"Contact","attr":"id"}]}],"name",[{"rel":"Contact","attr":"firstname"}]]}]
 	//var tdata='{"functions":{},"rules":[{"bind":{"Company":"Company","Contact":"Contact","table3":"table3","table2":"table2","table1":"table1"},"constraints":[],"yield":[{"atom":"ProductShape","args":[{"function":"f1","args":[{"rel":"Contact","attr":"id"}]}]},{"atom":"Triple","args":[[{"function":"f1","args":[{"rel":"Contact","attr":"id"}]}],"name",[{"rel":"Contact","attr":"firstname"}]]}]}]}'
-	$.ajax({
+	/*$.ajax({
         url: "chase",
         type: "POST",
         data:  JSON.stringify(editorJSON.get()),
@@ -50,9 +72,9 @@ exchange:function(e){
       })
       .always(function() {
         
-      });
+      });*/
 	
-}
+	}
 });
 
 var side_exchange_view = new sideExchangeView({ el: $("#result_container") });
